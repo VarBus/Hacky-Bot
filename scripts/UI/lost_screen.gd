907c04cb61d6dd62c21db4b3d3a35557a52c526d
leo-dev11 <U202213866@upc.edu.pe>
2025-11-08 15:16:20 -0500

    feature: Animation lost screen agregada

--- scripts/UI/lost_screen.gd
+++ scripts/UI/lost_screen.gd
@@ -1,9 +1,10 @@
 extends Control
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	animation_player.play("lost_screen_animation")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
